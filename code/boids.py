eecbea8f12ab3bd399775a79adf7ec21279031d1
Suze Frikkee <129743493+suus-stack@users.noreply.github.com>
2024-01-12 14:23:06 +0100

    boids implementation finished, but not working properly

--- code/boids.py
+++ code/boids.py
@@ -3,23 +3,24 @@ from  matplotlib import pyplot as plt
 
 
 def initialize_flock(nr_herring, lower_lim, upper_lim):
-    """ if you want the x-axis to vary between 100 and 200 and the y-axis to be betweeb
+    """ If you want the x-values to vary between 100 and 200 and the y-values to be between
     900 and 1100, you use: lower_lim = np.array([100, 900]) and upper_lim = np.array([200, 1100])"""
     width = upper_lim - lower_lim
     flock = lower_lim[:, np.newaxis] + np.random.rand(2, nr_herring) * width[:, np.newaxis]
+
     return flock
 
 def initialize_velocities(nr_herring, lower_lim, upper_lim):
-    """Random initializatio of velocitie for each herring."""
+    """Random initialization of velocitie for each herring."""
     width = upper_lim - lower_lim
     velocities = lower_lim[:, np.newaxis] + np.random.rand(2, nr_herring) * width[:, np.newaxis]
     return velocities 
 
 
 def update(positions, velocities):
-    min_distance = 70
-    # Initial movement being to the middle
-    attraction_to_center = 0.01
+    min_distance = 2000
+    # Repulsion of the center
+    attraction_to_center = 0.1
     center = np.mean(positions, 1) # 1 because along the horizontal axis
     # Calculating direction vectors from each position to the center
     # By adding a new axis the shape of center changes from (2,) to (2,1)
@@ -28,42 +29,21 @@ def update(positions, velocities):
     
     # Avoiding collision
     # Creating a 2 x N x N matrix of the distances between each herring
-    # distances_eucl = np.linalg.norm(positions[:, np.newaxis, :] - positions[:, :, np.newaxis], axis=0)
-    # print('eucladian distances', distances_eucl)
-    distances = positions[:, np.newaxis, :] - positions[:, :, np.newaxis] #?!
-    # print('distances', distances)
-    squared_distances = np.sum(distances**2, 0)
-    # print('squared_distances', squared_distances)
-    # # # Herring that are too close
-    # far_away = distances_eucl >= min_distance
-    # print('far away', far_away)
-    # # far_away = np.logical_not(close_herrings)
-    # # print(far_away.shape)
-    # # # Need to find the herrings that are too close and then swim away from them
-    # close_herrings = np.copy(distances_eucl)
-    # # close_herrings[0, :, :][far_away] = 0
-    # # close_herrings[1, :, :][far_away] = 0
-    # close_herrings[far_away] = 0
-    # print('close herrings', close_herrings)
-    # print(velocities.shape)
-    # print('sum', np.sum(close_herrings))
-    # velocities += np.sum(close_herrings, 1)
-    # print('velocities after change', velocities)
-    
-    separations = positions[:, np.newaxis, :] - positions[:, :, np.newaxis]
-    squared_displacements = separations * separations
-    square_distances = np.sum(squared_displacements, 0)
-    alert_distance = 100
-    far_away = square_distances > alert_distance
-    separations_if_close = np.copy(separations)
-    separations_if_close[0, :, :][far_away] = 0
-    separations_if_close[1, :, :][far_away] = 0
-    velocities += np.sum(separations_if_close, 1)
-    
-    
+    distances = positions[:, np.newaxis, :] - positions[:, :, np.newaxis] # all pairwise distances in x and y direction
+    squared_distances = np.sum(distances**2, 0)  # distance from 1 to 2 equals 2 to 1
+    far_away = squared_distances >= min_distance
+    close_herring = np.copy(distances)
+    # X-direction
+    close_herring[0, :, :][far_away] = 0
+    # Y-direction
+    close_herring[1, :, :][far_away] = 0
+
+    # Swimming away from the close herring
+    velocities += np.sum(close_herring, 1)
+
     velocity_differences = velocities[:, np.newaxis, :] - velocities[:, :, np.newaxis]
-    formation_flying_distance = 10000
-    formation_flying_strength = 0.125
+    formation_flying_distance = 10
+    formation_flying_strength = 0.5
     very_far = squared_distances > formation_flying_distance
     velocity_differences_if_close = np.copy(velocity_differences)
     velocity_differences_if_close[0, :, :][very_far] = 0
@@ -71,16 +51,14 @@ def update(positions, velocities):
     velocities -= np.mean(velocity_differences_if_close, 1) * formation_flying_strength
 
     
-    
     # Update all individual positions
     positions += velocities
-    positions %= 900
+    
 
 def visualize(positions, ax1):
     
     ax1.axis([0, 2000, 0, 2000])
     ax1.scatter(positions[0, :], positions[1, :], c='blue', alpha=0.5, marker='o', s=10)
-
     plt.draw()
     plt.pause(0.01)
     ax1.cla()
@@ -103,7 +81,7 @@ def run(iterations, positions, velocities):
 
 
 # USAGE 
-positions = initialize_flock(100, np.array([100, 900]), np.array([200, 1100]))
-velocities = initialize_velocities(100, np.array([0, -20]), np.array([10, 20]))
+positions = initialize_flock(10, np.array([100, 900]), np.array([200, 1000]))
+velocities = initialize_velocities(10, np.array([0, -20]), np.array([10, 20]))
 
-run(1000, positions, velocities)
+run(100, positions, velocities)
