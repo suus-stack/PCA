91c5e2084b3dbebab11d3a25ee690fd2ae9cf3e4
Suze Frikkee <129743493+suus-stack@users.noreply.github.com>
2024-01-15 16:52:13 +0100

    further boids implementation

--- code/boids.py
+++ code/boids.py
@@ -10,37 +10,64 @@ def initialize_flock(nr_herring, lower_lim, upper_lim):
 
     return flock
 
+def initialize_predator(nr_predators, lower_lim, upper_lim):
+    width = upper_lim - lower_lim
+    predator_positions = lower_lim[:, np.newaxis] + np.random.rand(2, nr_predators) * width[:, np.newaxis]
+
+    return predator_positions 
+
 def initialize_velocities(nr_herring, lower_lim, upper_lim):
     """Random initialization of velocitie for each herring."""
     width = upper_lim - lower_lim
     velocities = lower_lim[:, np.newaxis] + np.random.rand(2, nr_herring) * width[:, np.newaxis]
+
     return velocities 
 
-
-def update(positions, velocities):
-    min_distance = 2000
-    # Repulsion of the center
-    attraction_to_center = 0.1
+def center_movement(positions, velocities):
+    """Center of the mass attraction, like the need to get to the group"""
+    
+    # negative=repulsion, positive is attraction 
+    attraction_to_center = 0.006
     center = np.mean(positions, 1) # 1 because along the horizontal axis
     # Calculating direction vectors from each position to the center
     # By adding a new axis the shape of center changes from (2,) to (2,1)
-    direction_to_center = positions - center[:, np.newaxis]
-    velocities -= direction_to_center * attraction_to_center
+    direction_to_center = positions - center[:, np.newaxis] 
     
-    # Avoiding collision
-    # Creating a 2 x N x N matrix of the distances between each herring
-    distances = positions[:, np.newaxis, :] - positions[:, :, np.newaxis] # all pairwise distances in x and y direction
-    squared_distances = np.sum(distances**2, 0)  # distance from 1 to 2 equals 2 to 1
-    far_away = squared_distances >= min_distance
-    close_herring = np.copy(distances)
-    # X-direction
-    close_herring[0, :, :][far_away] = 0
-    # Y-direction
-    close_herring[1, :, :][far_away] = 0
+    velocities -= direction_to_center * attraction_to_center 
+    positions += velocities
+    positions[0] %= 2000
+    positions[1] %= 2000
+    
+   
 
-    # Swimming away from the close herring
-    velocities += np.sum(close_herring, 1)
+def cohesion(positions, velocities):
+    """Align positions."""
 
+    min_distance = 2000
+    alignment_distance = 3000
+
+    distances = normalize(positions[:, np.newaxis, :] - positions[:, :, np.newaxis]) # all pairwise distances in x and y direction
+    
+    squared_distances = np.sum(distances**2, 0) # distance from 1 to 2 equals 2 to 1 (eucladian distances)
+    too_close = squared_distances < min_distance
+    too_far = squared_distances > alignment_distance
+    # Creating an alignment matrix with the herring that are import for the direction 
+    alignment_herring = np.copy(distances)
+    alignment_herring[0, :, :][too_close] = 0
+    alignment_herring[1, :, :][too_close] = 0
+    alignment_herring[0, :, :][too_far] = 0
+    alignment_herring[1, :, :][too_far] = 0
+    
+    velocities -= (np.sum(alignment_herring, 1) / nr_herring)
+    positions += velocities
+    positions[0] %= 2000
+    positions[1] %= 2000
+
+def alignment(positions, velocities):
+
+    distances = normalize(positions[:, np.newaxis, :] - positions[:, :, np.newaxis]) # all pairwise distances in x and y direction
+    
+    squared_distances = np.sum(distances**2, 0) # distance from 1 to 2 equals 2 to 1 (eucladian distances)
     velocity_differences = velocities[:, np.newaxis, :] - velocities[:, :, np.newaxis]
     formation_flying_distance = 10
     formation_flying_strength = 0.5
@@ -48,15 +75,46 @@ def update(positions, velocities):
     velocity_differences_if_close = np.copy(velocity_differences)
     velocity_differences_if_close[0, :, :][very_far] = 0
     velocity_differences_if_close[1, :, :][very_far] = 0
-    velocities -= np.mean(velocity_differences_if_close, 1) * formation_flying_strength
+    velocities -= np.mean(velocity_differences_if_close, 1) * formation_flying_strength 
 
+
+def collision_avoidance(positions, velocities):
+    min_distance = 2000
+    # Creating a 2 x N x N matrix of the distances between each herring
+    # distances = normalize(positions[:, np.newaxis, :] - positions[:, :, np.newaxis]) # all pairwise distances in x and y direction
+    distances = positions[:, np.newaxis, :] - positions[:, :, np.newaxis]# all pairwise distances in x and y direction
+    squared_distances = np.sum(distances**2, 0) # distance from 1 to 2 equals 2 to 1 (eucladian distances)
+    
+    # making sure that the impact of herring far away is not taken into account 
+    far_away = squared_distances >= min_distance
+    close_herring = np.copy(distances)
+    # X-direction
+    close_herring[0, :, :][far_away] = 0
+    # Y-direction
+    close_herring[1, :, :][far_away] = 0
+   
+    # Swimming away from the close herring
+    velocities -= np.sum(close_herring, 1) 
     
     # Update all individual positions
     positions += velocities
+    positions[0] %= 2000
+    positions[1] %= 2000
+
+def normalize(vector):
+    magnitude = np.linalg.norm(vector)
+    
+    if magnitude > 0:
+        # Scale the vector to have a unit magnitude
+        return vector / magnitude
+    else:
+        # If the magnitude is 0, return the original vector
+        return vector
+
     
 
 def visualize(positions, ax1):
-    
+    # nu nog gehardcode, nog dynamisch maken
     ax1.axis([0, 2000, 0, 2000])
     ax1.scatter(positions[0, :], positions[1, :], c='blue', alpha=0.5, marker='o', s=10)
     plt.draw()
@@ -73,15 +131,38 @@ def setup_plot():
 
     return ax1
 
-def run(iterations, positions, velocities):
+def run(iterations, positions, velocities, positions2=None, velocities2=None):
+    
     ax1 = setup_plot()
-    for _ in range(iterations):
-        update(positions, velocities)
+    for i in range(iterations):
+        collision_avoidance(positions, velocities)
+        alignment(positions, velocities)
+        center_movement(positions, velocities)
+        cohesion(positions, velocities)
         visualize(positions, ax1)
+            
+        if not positions2 == None:
+            collision_avoidance(positions2, velocities2)
+            center_movement(positions2, velocities2)
+            alignment(positions2, velocities2)
+            cohesion(positions2, velocities2)
 
 
-# USAGE 
-positions = initialize_flock(10, np.array([100, 900]), np.array([200, 1000]))
-velocities = initialize_velocities(10, np.array([0, -20]), np.array([10, 20]))
 
-run(100, positions, velocities)
+# USAGE
+nr_herring = 20
+iterations = 150
+upper_lim = np.array([0, 100])
+positions  = initialize_flock(nr_herring, np.array([0, 100]), np.array([0, 100]))
+velocities = initialize_velocities(nr_herring, np.array([0, -20]), np.array([10, 20]))
+
+# second flock
+positions2  = initialize_flock(nr_herring, np.array([0, 50]), np.array([0, 50]))
+velocities2 = initialize_velocities(nr_herring, np.array([0, -20]), np.array([10, 20]))
+
+# predator 
+predator_positions = initialize_predator(2, np.array([0, 100]), np.array([0, 100]))
+
+# run(iterations, positions, velocities, positions2, velocities2, predator_positions)
+run(iterations, positions, velocities)
+
