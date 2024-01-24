162104ae82142098b554dc4548c795c36eb07131
Suze Frikkee <129743493+suus-stack@users.noreply.github.com>
2024-01-24 14:01:43 +0100

    Plot improvement

--- code/visualisation.py
+++ code/visualisation.py
@@ -364,13 +364,13 @@ def visualizing_perception_change(time_simulation):
         The number of seconds the simulation runs.
     """
     # no perception change
-    return_values_dict1 = Experiment(200, 5, 20, time_simulation, True, True, False, False, 32, 32, 6).run()
+    return_values_dict1 = Experiment(250, 4, 20, time_simulation, True, True, False, False, 32, 32, 6).run()
     # predator perception change
-    return_values_dict2 = Experiment(200, 5, 20, time_simulation, True, True, True, False, 32, 32, 6).run()
+    return_values_dict2 = Experiment(250, 4, 20, time_simulation, True, True, True, False, 32, 32, 6).run()
     # herring perception change
-    return_values_dict3 = Experiment(200, 5, 20, time_simulation, True, True, False, True, 32, 32, 6).run()
+    return_values_dict3 = Experiment(250, 4, 20, time_simulation, True, True, False, True, 32, 32, 6).run()
     # herring and predator perception change
-    return_values_dict4 = Experiment(200, 5, 20, time_simulation, True, True, True, True, 32, 32, 6).run()
+    return_values_dict4 = Experiment(250, 4, 20, time_simulation, True, True, True, True, 32, 32, 6).run()
 
     plt.style.use('seaborn')
     fig, axs = plt.subplots(2, 2)
@@ -381,17 +381,25 @@ def visualizing_perception_change(time_simulation):
     axs[0, 0].set_title('No perception change', fontsize=10)
     # predator perception change
     axs[0, 1].plot(return_values_dict2['Elapsed_time'], np.diff(return_values_dict2['Killed_herring_count_predator_perception_change']), 'tab:orange')
-    axs[0, 1].plot(return_values_dict2['Elapsed_time'], return_values_dict2['Perception_lenghts_predator'])
+    axs[0, 1].plot(return_values_dict2['Elapsed_time'], return_values_dict2['Perception_lenghts_predator'], 'tab:orange', linestyle='--', alpha=0.5, label='Perception length predator')
     axs[0, 1].set_title('Predator perception change', fontsize=10)
     # herring perception change (x-as:time, y-as: killed herring)
     axs[1, 0].plot(return_values_dict3['Elapsed_time'], np.diff(return_values_dict3['Killed_herring_count_herring_perception_change']), 'tab:green')
-    axs[1, 0].plot(return_values_dict3['Elapsed_time'], return_values_dict3['Perception_lenghts_herring'])
+    axs[1, 0].plot(return_values_dict3['Elapsed_time'], return_values_dict3['Perception_lenghts_herring'], 'tab:green', linestyle='--', alpha=0.5, label='Perception length herring')
     axs[1, 0].set_title('Herring perception change', fontsize=10)
     # both herring and predator perception change (herring count lists should be the same so does not matter which one you choose)
     axs[1, 1].plot(return_values_dict4['Elapsed_time'], np.diff(return_values_dict4['Killed_herring_count_herring_perception_change']), 'tab:red')
-    axs[1, 1].plot(return_values_dict4['Elapsed_time'], return_values_dict4['Perception_lenghts_herring'])
-    axs[1, 1].plot(return_values_dict4['Elapsed_time'], return_values_dict4['Perception_lenghts_predator'])
+    axs[1, 1].plot(return_values_dict4['Elapsed_time'], return_values_dict4['Perception_lenghts_herring'], 'tab:red', linestyle='--', alpha=0.5, label='Perception length herring')
+    axs[1, 1].plot(return_values_dict4['Elapsed_time'], return_values_dict4['Perception_lenghts_predator'], linestyle='--', alpha=0.5, label='Perception length predator')
     axs[1, 1].set_title('Predator and herring perception change', fontsize=10)
+
+    # axs[0, 1].legend()
+    # axs[1, 0].legend()
+    # axs[1, 1]. legend()
+    
+    # fig.legend()
+    fig.legend(loc='upper center', bbox_to_anchor=(0.5, 1.0), ncol=3, fontsize='small', framealpha=1)
+
 
     for ax in axs.flat:
         ax.set(xlabel= 'Elapsed time', ylabel= 'Number of killed herring')
@@ -425,7 +433,7 @@ if __name__ == "__main__":
     # influences_closeness_herring(20, 30)
 
     # Determine the influence of changes in the perception length
-    visualizing_perception_change(30)
+    visualizing_perception_change(20)
 
 
     # """Faster run???????"""
